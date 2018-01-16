e9acfc12146b0f296da93e96b5482750960f1567
ZengYi <zydding@qq.com>
2018-01-16 17:34:40 +0800

    初始化成功，静态页面开发完成，下一步计划>实现动态效果：1.显示行号2.btn动态效果3.实现粘贴数据的格式处理4.从后台取到数据，并显示5.提供修改数据功能以及批量修改数据6.保存数据7.刷新数据

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
 })
 export class AppComponent {
   title = 'app';
