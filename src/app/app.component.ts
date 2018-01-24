31c58f58d846527c89a006a169ec15f76cfb260a
ZengYi <zydding@qq.com>
2018-01-24 09:06:04 +0800

    完成行号功能，完成webpack代理访问接口

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,70 @@
 import { Component,Directive,ElementRef,HostListener,Input } from '@angular/core';
+import { Http,Headers } from '@angular/http';
+
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   
 })
 export class AppComponent {
-  title = 'app';
   values='';
   cols=0;
   heroes=[];
-  i=0;
-  html='';
-  contentChange(value:string,obj,lines){
-    this.values=value+' | ';
-    this.cols=obj.value.split("\n").length;
-    if(this.cols>=15){
-      lines.style.marginTop='-'+(this.cols-15)*20+'px';
-      
-      for(this.i=0;this.i<=this.cols;this.i++){
-        this.html+='<div class="lineno">'+this.i+'</div>';
-      }
-      lines.innerHTML=this.html;
-    }else{
-      // for(this.i<=15;this.i++;){
-
-      // }
+  scrollHeight=0;
+  constructor(private http:Http) {
+    //初始化行号
+    for(var j=0;j<15;j++){
+      this.heroes.push('<div class="lineno">'+(j+1)+'</div>');
     }
   }
-  constructor(private el: ElementRef) {
-        
+  /**
+   * keyUp 事件
+   * 在输入框内容做改变时触发
+   * 做行号改变；获取输入框值
+   * logic逻辑：获取textarea的滚动条高度，打印相同长度的列表，在页面循环列表，显示行号,通过margin-top以及hidden属性来显示15行
+   */
+  contentChange(objTextarea,lines){
+    //this.cols=objTextarea.value.split("\n").length;
+    this.scrollHeight=objTextarea.scrollHeight-300;
+    this.cols=this.scrollHeight/20;
+    this.heroes=[];
+    if(this.cols>=1){
+      for(var i=0;i<this.cols+15;i++){
+        this.heroes.push('<div class="lineno">'+(i+1)+'</div>');
+      }
+      lines.style.marginTop='-'+(this.cols)*20+'px';
+      //console.log("这是大于15"+this.heroes+'列表长度:'+this.heroes.length+'当前行数：'+this.cols);
+    }else{
+      //小于15
+      for(var i=0;i<15;i++){
+        this.heroes.push('<div class="lineno">'+(i+1)+'</div>');
+      }
+      lines.style.marginTop='0px';
+      //console.log('显示行数:'+this.heroes.length+'当前行数：'+this.cols);
+    }
   }
-  color:string;
+  /**
+   * 获取头信息
+   */
+  getHeaders (){
+    const headers= new Headers({'Accept': 'application/json'});
+    return headers;
+  }
+  /**
+   * 通过输入框的值获取服务器的数据
+   * 
+   * 服务器通过webpack代理方式通过cookie验证访问
+   * @param boxValue 输入框的值
+   */
+  getList(boxValue){
+    this.http.get("/batch-info",{headers:this.getHeaders()}).toPromise()
+    .then(respone=>respone.json())
+    .catch(err=>{
+      console.log(err);
+      return Promise.reject(err);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
-import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
